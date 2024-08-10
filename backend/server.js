75c9db5946c031925162a4f67c7ff6035476b28b
jamestcole <jamestcole@yahoo.co.uk>
2024-08-10 15:22:58 +0100

    main

--- backend/server.js
+++ backend/server.js
@@ -65,4 +65,7 @@ app.get('/applications', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
